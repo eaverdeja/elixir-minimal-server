5b40743824809bec5fb51c44d97a5391bb53ace5
Eduardo Verdeja <eaverdeja@gmail.com>
2019-05-01 11:44:47 -0300

    Cleanup

--- lib/minimal_server.ex
+++ lib/minimal_server.ex
@@ -1,18 +1,2 @@
 defmodule MinimalServer do
-  @moduledoc """
-  Documentation for MinimalServer.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MinimalServer.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- lib/minimal_server/router.ex
+++ lib/minimal_server/router.ex
@@ -4,16 +4,18 @@ defmodule MinimalServer.Router do
   plug(:match)
   plug(:dispatch)
 
+  @content_type "application/json"
+
   get "/" do
     conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(200, Poison.encode!(message()))
+    |> put_resp_content_type(@content_type)
+    |> send_resp(200, message())
   end
 
   defp message do
-    %{
+    Poison.encode!(%{
       response_type: "in_channel",
       text: "Hello Elixir world!"
-    }
+    })
   end
 end
