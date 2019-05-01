d5cc04f1b9231b2693e6ec901330259b6d42c893
Eduardo Verdeja <eaverdeja@gmail.com>
2019-05-01 08:29:34 -0300

    Initial commit

---
+++ lib/minimal_server/application.ex
@@ -0,0 +1,19 @@
+defmodule MinimalServer.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      MinimalServer.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: MinimalServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ lib/minimal_server/endpoint.ex
@@ -0,0 +1,34 @@
+defmodule MinimalServer.Endpoint do
+  use Plug.Router
+  require Logger
+
+  plug(:match)
+
+  plug(Plug.Parsers,
+    parsers: [:json],
+    pass: ["applcation/json"],
+    json_decoder: Poison
+  )
+
+  plug(:dispatch)
+
+  forward("/bot", to: MinimalServer.Router)
+
+  match _ do
+    send_resp(conn, 404, "Requested page not found!")
+  end
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]}
+    }
+  end
+
+  def start_link(_opts) do
+    with {:ok, [port: port] = config} <- Application.fetch_env!(:minimal_server, __MODULE__) do
+      Logger.info("starting server at http://localhost:#{port}/")
+      Plug.Cowboy.http(__MODULE__, [], config)
+    end
+  end
+end
